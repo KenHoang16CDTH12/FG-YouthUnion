04c2b530df057e68bea83f9eaa8bf866d0a117c7
Duong Minh Hoang <angelkittybihoang@gmail.com>
2018-08-16 18:18:26 +0700

    Create migrate

---
+++ database/migrations/2018_08_14_093926_create_classes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('desc')->nullable();
+            $table->integer('khoa_id')->unsigned();
+            $table->timestamps();
+            //FK
+            $table->foreign('khoa_id')->references('id')->on('khoas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+}
---
+++ database/migrations/2018_08_14_094243_create_user_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 50);
+            $table->string('middle_name', 50);
+            $table->string('last_name', 50);
+            $table->boolean('gender');
+            $table->date('date_of_birth')->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->text('address')->nullable();
+            $table->text('photo')->nullable();
+            $table->string('student_code', 50)->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+            //FK
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}
