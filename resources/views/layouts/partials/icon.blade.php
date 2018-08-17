a21289cc03bc87fd73a761a2e1e4e6b8205c02a1
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-08-17 18:41:31 +0700

    design_login_page

---
+++ resources/views/layouts/partials/icon.blade.php
@@ -0,0 +1,16 @@
+<!-- ============================================ -->
+<!-- Icon, font -->
+<!-- ============================================ -->
+<link rel="apple-touch-icon" sizes="60x60" href="{{ URL::asset('app-assets/images/ico/apple-icon-60.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('app-assets/images/ico/apple-icon-76.png') }}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset('app-assets/images/ico/apple-icon-120.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset('app-assets/images/ico/apple-icon-152.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('app-assets/images/ico/favicon.ico') }}">
+<link rel="shortcut icon" type="image/png" href="{{ URL::asset('app-assets/images/ico/favicon-32.png') }}">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+rel="stylesheet">
+<link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
+rel="stylesheet">
+<!-- ============================================ -->
+<!-- Icon, font -->
+<!-- ============================================ -->
