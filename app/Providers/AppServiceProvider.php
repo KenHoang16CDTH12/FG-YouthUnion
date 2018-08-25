25c58452535e2f39fde670a4ad59d60245ddb11c
Hoàng Bảo Vũ <baovuhbv@gmail.com>
2018-08-26 00:15:04 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('REDIRECT_HTTPS'))
-        {
-            $url->forceSchema('https');
-        }
+        //
     }
 
     /**
