efa8d1fbb806e542f6c25f4e035734c0efedfe99
Thao <huynhngoathao@gmail.com>
2018-08-25 15:43:03 +0700

    create UserHoatDongsTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,6 +24,6 @@ class DatabaseSeeder extends Seeder
         HocKysTableSeeder::class,
         HoatDongTypesTableSeeder::class
         );
-
+        $this->call(UserHoatDongsTableSeeder::class);
     }
 }
