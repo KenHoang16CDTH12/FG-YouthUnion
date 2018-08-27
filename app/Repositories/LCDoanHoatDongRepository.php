3f738d67dc5f1354b32164484cde289267246897
Thao <huynhngoathao@gmail.com>
2018-08-27 09:02:00 +0700

    create LCDoanHoatDongController ver 2

---
+++ app/Repositories/LCDoanHoatDongRepository.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Repositories;
+
+use App\LCDoanHoatDong;
+use App\Http\Resources\LCDoanHoatDongResource;
+
+class LCDoanHoatDongRepository
+{
+    /**
+     * Get all of the objects for a given model.
+     *
+     * @return Collection
+     */
+    public function collection()
+    {
+        // Return collection of objects as a resource
+        return LCDoanHoatDongResource::collection(LCDoanHoatDong::orderBy('created_at', 'desc')->paginate(25));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Return object
+        return new LCDoanHoatDongResource(User::findOrFail($id));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store($request)
+    {
+        // Return object
+        return new LCDoanHoatDongResource(LCDoanHoatDong::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $request | $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($request, $id)
+    {
+        $lcdoanhoatdong = LCDoanHoatDong::findOrFail($id);
+        $lcdoanhoatdong->update($request->only(['active', 'role_id', 'class_id']));
+        // Return object
+        return new LCDoanHoatDongResource($lcdoanhoatdong);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $lcdoanhoatdong = LCDoanHoatDong::findOrFail($id);
+      $lcdoanhoatdong->delete();
+      return response()->json([
+          'meesage' => 'Delete #' . $id . ' successful!'
+      ], 200);
+    }
+}
