e7400e71ea9924d638520ccab8a99fd2c3dfcaee
quoctuan1998 <quoctuan1998@gmail.com>
2018-08-27 14:20:17 +0700

    Edit lcdoan controller

---
+++ app/Http/Requests/LCDoanStoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LCDoanStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the lcdoan is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
