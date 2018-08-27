89a137b56eab448354a6e5874be4bab57a2acc5b
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-08-27 20:54:13 +0700

    Edit design_card_menu V2

--- resources/views/admin/dashboard/partials/card_pull_up.blade.php
+++ resources/views/admin/dashboard/partials/card_pull_up.blade.php
@@ -1,70 +1,8 @@
 <!-- eCommerce statistic -->
 <div class="row">
-  <div class="col-xl-4 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="purple">0</h3>
-              <h6>Federation</h6>
-            </div>
-            <div>
-              <i class="icon-link purple font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-purple" role="progressbar" style="width: 80%"
-            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-xl-4 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="red">0</h3>
-              <h6>Faculty</h6>
-            </div>
-            <div>
-              <i class="icon-grid red font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-red" role="progressbar" style="width: 65%"
-            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-xl-4 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="teal">0</h3>
-              <h6>Roles</h6>
-            </div>
-            <div>
-              <i class="icon-directions teal font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-teal" role="progressbar" style="width: 65%"
-            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
+  <!-- ============================================ -->
+  <!-- BEGIN CARD USERS -->
+  <!-- ============================================ -->
   <div class="col-xl-3 col-lg-6 col-12">
     <div class="card pull-up">
       <div class="card-content">
@@ -72,127 +10,26 @@
           <div class="media d-flex">
             <div class="media-body text-left">
               <h3 class="info">0</h3>
-              <h6>Users</h6>
+              <h6>Người Dùng</h6>
             </div>
             <div>
               <i class="icon-users info font-large-2 float-right"></i>
             </div>
           </div>
           <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 80%"
+            <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 100%"
             aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="col-xl-3 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="warning">0</h3>
-              <h6>Years</h6>
-            </div>
-            <div>
-              <i class="icon-graduation warning font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 65%"
-            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-xl-3 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="success">0</h3>
-              <h6>Terms</h6>
-            </div>
-            <div>
-              <i class="icon-calendar success font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-success" role="progressbar" style="width: 75%"
-            aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-xl-3 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="danger">0</h3>
-              <h6>Activities</h6>
-            </div>
-            <div>
-              <i class="icon-badge danger font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 85%"
-            aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
-  <div class="col-xl-3 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="cyan">0</h3>
-              <h6>Federation Activities</h6>
-            </div>
-            <div>
-              <i class="icon-trophy cyan font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-cyan" role="progressbar" style="width: 80%"
-            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-xl-3 col-lg-6 col-12">
-    <div class="card pull-up">
-      <div class="card-content">
-        <div class="card-body">
-          <div class="media d-flex">
-            <div class="media-body text-left">
-              <h3 class="pink">0</h3>
-              <h6>User Activities</h6>
-            </div>
-            <div>
-              <i class="icon-fire pink font-large-2 float-right"></i>
-            </div>
-          </div>
-          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-pink" role="progressbar" style="width: 65%"
-            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <!-- ============================================ -->
+  <!-- END CARD USERS -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- BEGIN CARD CLASS -->
+  <!-- ============================================ -->
   <div class="col-xl-3 col-lg-6 col-12">
     <div class="card pull-up">
       <div class="card-content">
@@ -200,39 +37,183 @@
           <div class="media d-flex">
             <div class="media-body text-left">
               <h3 class="teal">0</h3>
-              <h6>Class</h6>
+              <h6>Lớp</h6>
             </div>
             <div>
               <i class="icon-layers teal font-large-2 float-right"></i>
             </div>
           </div>
           <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-teal " role="progressbar" style="width: 75%"
+            <div class="progress-bar bg-gradient-x-teal " role="progressbar" style="width: 100%"
             aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <!-- ============================================ -->
+  <!-- END CARD CLASS -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- BEGIN CARD FACULTY -->
+  <!-- ============================================ -->
   <div class="col-xl-3 col-lg-6 col-12">
     <div class="card pull-up">
       <div class="card-content">
         <div class="card-body">
           <div class="media d-flex">
             <div class="media-body text-left">
-              <h3 class="yellow">0</h3>
-              <h6>Activities Type</h6>
+              <h3 class="red">0</h3>
+              <h6>Khoa</h6>
             </div>
             <div>
-              <i class="icon-puzzle yellow font-large-2 float-right"></i>
+              <i class="icon-grid red font-large-2 float-right"></i>
             </div>
           </div>
           <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-            <div class="progress-bar bg-gradient-x-yellow" role="progressbar" style="width: 85%"
+            <div class="progress-bar bg-gradient-x-red" role="progressbar" style="width: 100%"
+            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================ -->
+  <!-- END CARD FACULTY -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- BEGIN CARD FEDERATION -->
+  <!-- ============================================ -->
+  <div class="col-xl-3 col-lg-6 col-12">
+    <div class="card pull-up">
+      <div class="card-content">
+        <div class="card-body">
+          <div class="media d-flex">
+            <div class="media-body text-left">
+              <h3 class="purple">0</h3>
+              <h6>Liên Chi Đoàn</h6>
+            </div>
+            <div>
+              <i class="icon-link purple font-large-2 float-right"></i>
+            </div>
+          </div>
+          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+            <div class="progress-bar bg-gradient-x-purple" role="progressbar" style="width: 100%"
+            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================ -->
+  <!-- END CARD FEDERATION -->
+  <!-- ============================================ -->
+</div>
+<div class="row">
+  <!-- ============================================ -->
+  <!-- BEGIN CARD YEARS-->
+  <!-- ============================================ -->
+  <div class="col-xl-3 col-lg-6 col-12">
+    <div class="card pull-up">
+      <div class="card-content">
+        <div class="card-body">
+          <div class="media d-flex">
+            <div class="media-body text-left">
+              <h3 class="warning">0</h3>
+              <h6>Năm Học</h6>
+            </div>
+            <div>
+              <i class="icon-graduation warning font-large-2 float-right"></i>
+            </div>
+          </div>
+          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+            <div class="progress-bar bg-gradient-x-warning" role="progressbar" style="width: 100%"
+            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================ -->
+  <!-- END CARD YEARS -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- BEGIN CARD ACTIVITIES -->
+  <!-- ============================================ -->
+  <div class="col-xl-3 col-lg-6 col-12">
+    <div class="card pull-up">
+      <div class="card-content">
+        <div class="card-body">
+          <div class="media d-flex">
+            <div class="media-body text-left">
+              <h3 class="danger">0</h3>
+              <h6>Hoạt Động</h6>
+            </div>
+            <div>
+              <i class="icon-badge danger font-large-2 float-right"></i>
+            </div>
+          </div>
+          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+            <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 100%"
             aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <!-- ============================================ -->
+  <!-- END CARD ACTIVITIES -->
+  <!-- ============================================ -->
+  <!-- ============================================ -->
+  <!-- BEGIN CARD FEDERATION -->
+  <!-- ============================================ -->
+  <div class="col-xl-3 col-lg-6 col-12">
+    <div class="card pull-up">
+      <div class="card-content">
+        <div class="card-body">
+          <div class="media d-flex">
+            <div class="media-body text-left">
+              <h3 class="cyan">0</h3>
+              <h6>Sự Kiện</h6>
+            </div>
+            <div>
+              <i class="icon-trophy cyan font-large-2 float-right"></i>
+            </div>
+          </div>
+          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+            <div class="progress-bar bg-gradient-x-cyan" role="progressbar" style="width: 100%"
+            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================ -->
+  <!-- BEGIN CARD FEDERATION ACTIVITIES -->
+  <!-- ============================================ -->
+  <div class="col-xl-3 col-lg-6 col-12">
+    <div class="card pull-up">
+      <div class="card-content">
+        <div class="card-body">
+          <div class="media d-flex">
+            <div class="media-body text-left">
+              <h3 class="pink">0</h3>
+              <h6>Điểm Danh</h6>
+            </div>
+            <div>
+              <i class="icon-fire pink font-large-2 float-right"></i>
+            </div>
+          </div>
+          <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+            <div class="progress-bar bg-gradient-x-pink" role="progressbar" style="width: 100%"
+            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================ -->
+  <!-- END CARD FEDERATION ACTIVITIES-->
+  <!-- ============================================ -->
 </div>
