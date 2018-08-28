f8c542b48d584669d2c103f64a6da2f66115635d
BinhNguyen <nguyenvanbinh3479@gmail.com>
2018-08-28 15:08:20 +0700

    Update UserHoatDongStoreRequest.php

--- app/Http/Requests/UserHoatDongStoreRequest.php
+++ app/Http/Requests/UserHoatDongStoreRequest.php
@@ -26,7 +26,7 @@ class UserHoatDongStoreRequest extends FormRequest
         return [
             'user_id' => 'required|integer',
             'hoatdongs_id' => 'required|integer',
-            'status' => 'required|string|status|max:45'
+            'status' => 'required|string|max:45'
         ];
     }
 }
