0753caf1fc4c83726c1cf7f07e11cd122375cfa6
Binh Nguyen <nguyenvanbinh3479@gmail.com>
2018-09-08 09:25:06 +0700

    design form example

--- resources/assets/js/pages/dashboards/Users.vue
+++ resources/assets/js/pages/dashboards/Users.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="content-body">
+    <form-example/>
     <card-table-user></card-table-user>
   </div>
 </template>
 <script>
 import PullUpCard from './PullUpCard.vue';
 import CardTableUser from '../users/CardTable.vue';
+import FormExample from '../../components/form-element/FormExample.vue';
 
 export default {
     name: 'dashboard',
-    components: { PullUpCard, CardTableUser }
+    components: { PullUpCard, CardTableUser, FormExample }
 }
 </script>
